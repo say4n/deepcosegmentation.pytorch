0a60ae3f8c140460636d18500777626bb88fb039
Sayan Goswami <goswami.sayan47@gmail.com>
2018-06-18 12:46:29 +0530

    Mutual correlation - implemented

--- src/model.py
+++ src/model.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 
-DEBUG = False
+DEBUG = True
 
 
 vgg16_dims = [
@@ -35,6 +35,18 @@ class SegNet(nn.Module):
         self.output_channels = output_channels      # FG + BG = 2
 
 
+        def encoder_blocks(input_channels, output_channels, batch_norm=True):
+            layers = []
+            layers.append(nn.Conv2d(input_channels,
+                                    output_channels,
+                                    kernel_size=3,
+                                    padding=1))
+            if batch_norm:
+                layers.append(nn.BatchNorm2d(output_channels))
+
+            return layers
+
+
         def decoder_blocks(input_channels, output_channels, batch_norm=True):
             layers = []
             layers.append(nn.ConvTranspose2d(input_channels,
@@ -48,45 +60,87 @@ class SegNet(nn.Module):
         
 
         self.encoder = models.vgg16(pretrained=True).features
-        self.decoder = nn.Sequential(nn.MaxUnpool2d(kernel_size=2, stride=2),
-                                     *decoder_blocks(961 + 512, 512),
+        self.encoder_l2 = nn.Sequential(*encoder_blocks(512, 1024),
+                                        *encoder_blocks(1024, 1024))
+
+        self.decoder = nn.Sequential(nn.Upsample(scale_factor=2, mode='nearest'),
+                                     *decoder_blocks(1024 + 1024, 512),
                                      *decoder_blocks(512, 512),
                                      *decoder_blocks(512, 512),
-                                     nn.MaxUnpool2d(kernel_size=2, stride=2),
+                                     nn.Upsample(scale_factor=2, mode='nearest'),
                                      *decoder_blocks(512, 512),
                                      *decoder_blocks(512, 512),
                                      *decoder_blocks(512, 512),
-                                     nn.MaxUnpool2d(kernel_size=2, stride=2),
+                                     nn.Upsample(scale_factor=2, mode='nearest'),
                                      *decoder_blocks(512, 256),
                                      *decoder_blocks(256, 256),
                                      *decoder_blocks(256, 256),
-                                     nn.MaxUnpool2d(kernel_size=2, stride=2),
+                                     nn.Upsample(scale_factor=2, mode='nearest'),
                                      *decoder_blocks(256, 128),
                                      *decoder_blocks(128, 128),
-                                     nn.MaxUnpool2d(kernel_size=2, stride=2),
+                                     nn.Upsample(scale_factor=2, mode='nearest'),
                                      *decoder_blocks(128, 64),
                                      *decoder_blocks(64, self.output_channels))
 
     
     def compute_correlation(self, featureA, featureB):
-        pass
+        # https://github.com/pytorch/pytorch/issues/4073
+        B = featureA.shape[0]
+        W = featureA.shape[-1]
+        H = featureA.shape[-2]
+
+        D = 2*max(W, H)
+        patch_size = 3
+
+        fA = featureA.transpose(1, 2).transpose(2, 3)       # B, H, W, C
+        fB = featureB.transpose(1, 2).transpose(2, 3)       # B, H, W, C
+
+        cAB = torch.zeros((B, W, H, D**2))                  # B, H, W, D^2
+
+        for b in range(B):
+            for i in range(H):
+                for j in range(W):
+                    for dm in range(-patch_size//2, patch_size//2 + 1):
+                        for dn in range(-patch_size//2, patch_size//2 + 1):
+                            m, n, k = i + dm, j + dn, dn * D + dm
+
+                            if 0 <= m < H and 0 <= n < W:
+                                cAB[b, i, j] += fA[b, i, j] * fB[b, m, n] 
+        
+        return cAB.transpose_(3, 2).transpose_(2, 1)        # B, D^2, H, W 
 
 
     def forward(self, imageA, imageB):
         """
         Forward pass images through the network
         """
-        featureA = self.encoder(imageA)
-        featureB = self.encoder(imageB)
+        featureA = self.encoder_l2(self.encoder(imageA))
+        featureB = self.encoder_l2(self.encoder(imageB))
 
-        correlationAB = compute_correlation(featureA, featureB)
-        correlationBA = compute_correlation(featureB, featureA)
+        if DEBUG:
+            print(f"featureA.size(): {featureA.size()}")
+            print(f"featureB.size(): {featureB.size()}")
 
-        correspondence_mapA = torch.cat([featureA, correlationAB])
-        correspondence_mapB = torch.cat([featureB, correlationBA])
+        correlationAB = self.compute_correlation(featureA, featureB)
+        correlationBA = self.compute_correlation(featureB, featureA)
+
+        if DEBUG:
+            print(f"correlationAB.size(): {correlationAB.size()}")
+            print(f"correlationBA.size(): {correlationBA.size()}")
+
+        correspondence_mapA = torch.cat([featureA, correlationAB], dim=1)
+        correspondence_mapB = torch.cat([featureB, correlationBA], dim=1)
+
+        if DEBUG:
+            print(f"correspondence_mapA.size(): {correspondence_mapA.size()}")
+            print(f"correspondence_mapB.size(): {correspondence_mapB.size()}")
 
         probability_mapA = self.decoder(correspondence_mapA)
         probability_mapB = self.decoder(correspondence_mapB)
+
+        if DEBUG:
+            print(f"probability_mapA.size(): {probability_mapA.size()}")
+            print(f"probability_mapB.size(): {probability_mapB.size()}")
 
         return probability_mapA, probability_mapB
 
@@ -94,7 +148,7 @@ class SegNet(nn.Module):
 if __name__ == "__main__":
     model = SegNet(input_channels=3, output_channels=2)
 
-    fA = torch.rand((4, 1024, 16, 16))
-    fB = torch.rand((4, 1024, 16, 16))
+    iA = torch.rand((1, 3, 512, 512))
+    iB = torch.rand((1, 3, 512, 512))
 
-    print(model.compute_correlation(fA, fB).size())
+    pmaps = model(iA, iB)
