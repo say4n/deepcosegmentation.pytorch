c56153848d00d284bb3cbed2fa040dd333b9c7b5
Sayan Goswami <goswami.sayan47@gmail.com>
2018-06-21 18:33:01 +0530

    train

--- src/train.py
+++ src/train.py
@@ -38,16 +38,68 @@ BETAS = (0.9, 0.999)
 ## Dataset
 BATCH_SIZE = 2 * 32 # two images at a time for Siamese net
 INPUT_CHANNELS = 3 # RGB
-OUTPUT_CHANNELS = 1 # Binary mask
+OUTPUT_CHANNELS = 2 # BG + FG channel
 
 ## Training
 CUDA = args.gpu
 CHECKPOINT = args.checkpoint_save_dir
 LOAD_CHECKPOOINT = args.checkpoint_load_dir
+NUM_EPOCHS = 1
 
 
 def train():
-    pass
+    is_better = True
+    prev_loss = float('inf')
+
+    model.train()
+
+    for epoch in range(NUM_EPOCHS):
+        loss_f = 0
+        t_start = time.time()
+
+        for batch_idx, batch in enumerate(dataloader):
+            images = batch["image"]
+            labels = batch["label"]
+            masks  = batch["mask"]
+
+            pairwise_images = FloatTensor([(images[2*idx], images[2*idx+1]) for idx in range(BATCH_SIZE//2)])
+            pairwise_labels = LongTensor([(labels[2*idx], labels[2*idx+1]) for idx in range(BATCH_SIZE//2)])
+            pairwise_masks  = FloatTensor([(masks[2*idx], masks[2*idx+1]) for idx in range(BATCH_SIZE//2)])
+
+            imagesA, imagesB = zip(*pairwise_images)
+            labelsA, labelsB = zip(*pairwise_labels)
+            masksA, masksB = zip(*pairwise_masks)
+
+            pmapA, pmapB = model(imagesA, imagesB)
+
+            optimizer.zero_grad()
+
+            lossA = loss(pmapA, masksA)
+            lossB = loss(pmapB, masksB)
+
+            loss = lossA + lossB
+
+            loss.backward()
+
+            optimizer.step()
+
+
+            # Add losses for epoch
+            loss_f += loss.float()
+
+        delta = time.time() - t_start
+        is_better = loss_f < prev_loss
+
+
+        if is_better:
+            prev_loss = loss_f
+
+            if CHECKPOINT:
+                torch.save(model.state_dict(), os.path.join(CHECKPOINT, "model_best.pth"))
+
+        print("Epoch #{}\tLoss: {:.8f}\t Time: {:2f}s".format(epoch+1, loss_f, delta))
+
+
 
 
 if __name__ == "__main__":
@@ -68,8 +120,12 @@ if __name__ == "__main__":
 
 
     model = SiameseSegNet(input_channels=INPUT_CHANNELS, output_channels=OUTPUT_CHANNELS)
-    loss = nn.BCELoss()
-    optimiser = torch.optim.Adam(lr=LEARNING_RATE, betas=BETAS)
+
+    loss = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(lr=LEARNING_RATE, betas=BETAS)
+
+    FloatTensor = torch.FloatTensor
+    LongTensor = torch.LongTensor
 
     if CUDA:
         os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
@@ -77,8 +133,11 @@ if __name__ == "__main__":
         model.cuda()
         loss.cuda()
 
+        FloatTensor = torch.cuda.FloatTensor
+        LongTensor = torch.cuda.LongTensor
+
     if LOAD_CHECKPOOINT:
-        model.load_state_dict(torch.load(LOAD_CHECKPOOINT))
+        model.load_state_dict(torch.load(LOAD_CHECKPOINT))
 
 
     train()
