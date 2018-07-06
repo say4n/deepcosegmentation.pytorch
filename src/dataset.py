6e540e75bf922a9009c9cb2909d515dfbde9a6cd
Sayan Goswami <goswami.sayan47@gmail.com>
2018-07-06 16:04:42 +0530

    Add MSRC & Internet dataset
    ABC for Datasets!

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,4 @@
-"""iCoseg Dataset"""
+"""Object Co-segmentation Datasets"""
 
 
 import numpy as np
@@ -8,13 +8,50 @@ from torchvision.datasets import ImageFolder, DatasetFolder
 from torchvision.transforms import ToTensor
 
 
-class iCosegDataset(Dataset):
-    def __init__(self, *, image_dir, mask_dir):
+class DatasetABC(Dataset):
+    """Abstract Base Class for Datasets"""
+    def __init__(self, image_dir, mask_dir):
         self.image_dir = image_dir
         self.mask_dir = mask_dir
 
         self.img_size = (512, 512)
         self.in_channels = 3
+
+        self.image_data = None
+        self.mask_data = None
+
+        self.length = None
+
+    def image_loader(self, path):
+        raise NotImplementedError("`image_loader` not implemented.")
+
+    def mask_loader(self, path):
+        raise NotImplementedError("`image_loader` not implemented.")
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        image, im_label = self.image_data[index]
+        mask, ma_label = self.mask_data[index]
+
+        label = None
+
+        if im_label == ma_label:
+            label = im_label
+
+        data = {
+            "image": image,
+            "mask" : mask,
+            "label": label
+        }
+
+        return data
+
+
+class iCosegDataset(DatasetABC):
+    def __init__(self, *, image_dir, mask_dir):
+        super().__init__(image_dir, mask_dir)
 
         self.image_data = DatasetFolder(root=image_dir,
                                         loader=self.image_loader,
@@ -39,34 +76,10 @@ class iCosegDataset(Dataset):
 
         return img
 
-    def __len__(self):
-        return self.length
 
-    def __getitem__(self, index):
-        image, im_label = self.image_data[index]
-        mask, ma_label = self.mask_data[index]
-
-        label = None
-
-        if im_label == ma_label:
-            label = im_label
-
-        data = {
-            "image": image,
-            "mask" : mask,
-            "label": label
-        }
-
-        return data
-
-
-class PASCALVOCCosegDataset(Dataset):
+class PASCALVOCCosegDataset(DatasetABC):
     def __init__(self, *, image_dir, mask_dir):
-        self.image_dir = image_dir
-        self.mask_dir = mask_dir
-
-        self.img_size = (512, 512)
-        self.in_channels = 3
+        super().__init__(image_dir, mask_dir)
 
         self.image_data = DatasetFolder(root=image_dir,
                                         loader=self.image_loader,
@@ -91,25 +104,61 @@ class PASCALVOCCosegDataset(Dataset):
 
         return img
 
-    def __len__(self):
-        return self.length
 
-    def __getitem__(self, index):
-        image, im_label = self.image_data[index]
-        mask, ma_label = self.mask_data[index]
+class InternetDataset(DatasetABC):
+    def __init__(self, *, image_dir, mask_dir):
+        super().__init__(image_dir, mask_dir)
 
-        label = None
+        self.image_data = DatasetFolder(root=image_dir,
+                                        loader=self.image_loader,
+                                        extensions=["jpg"],
+                                        transform=ToTensor())
 
-        if im_label == ma_label:
-            label = im_label
+        self.mask_data = DatasetFolder(root=mask_dir,
+                                       loader=self.mask_loader,
+                                       extensions=["png"])
 
-        data = {
-            "image": image,
-            "mask" : mask,
-            "label": label
-        }
+        self.length = len(self.image_data)
 
-        return data
+    def image_loader(self, path):
+        img = Image.open(path).resize(self.img_size)
+        img = np.array(img).astype(np.float32)/255.0
+
+        return img
+
+    def mask_loader(self, path):
+        img = Image.open(path).convert('1').resize(self.img_size)
+        img = np.array(img).astype(np.uint8)
+
+        return img
+
+
+class MSRCDataset(DatasetABC):
+    def __init__(self, *, image_dir, mask_dir):
+        super().__init__(image_dir, mask_dir)
+
+        self.image_data = DatasetFolder(root=image_dir,
+                                        loader=self.image_loader,
+                                        extensions=["bmp"],
+                                        transform=ToTensor())
+
+        self.mask_data = DatasetFolder(root=mask_dir,
+                                       loader=self.mask_loader,
+                                       extensions=["bmp"])
+
+        self.length = len(self.image_data)
+
+    def image_loader(self, path):
+        img = Image.open(path).resize(self.img_size)
+        img = np.array(img).astype(np.float32)/255.0
+
+        return img
+
+    def mask_loader(self, path):
+        img = Image.open(path).convert('1').resize(self.img_size)
+        img = np.array(img).astype(np.uint8)
+
+        return img
 
 
 if __name__ == "__main__":
@@ -117,6 +166,17 @@ if __name__ == "__main__":
                                    mask_dir="/Users/Sayan/Desktop/Research/IIT B/Vision/datasets/iCoseg/ground_truth")
     print(f"iCoseg_dataset: # samples = {len(iCoseg_dataset)}")
 
+
     PASCALVOCCoseg_dataset = PASCALVOCCosegDataset(image_dir="/Users/Sayan/Desktop/Research/IIT B/Vision/datasets/PASCAL_coseg/images",
                                                    mask_dir="/Users/Sayan/Desktop/Research/IIT B/Vision/datasets/PASCAL_coseg/GT")
     print(f"PASCALVOCCoseg_dataset: # samples = {len(PASCALVOCCoseg_dataset)}")
+
+
+    Internet_dataset = InternetDataset(image_dir="/Users/Sayan/Desktop/Research/IIT B/Vision/datasets/internet_dataset_ObjectDiscovery-data/internet_processed/images",
+                                       mask_dir="/Users/Sayan/Desktop/Research/IIT B/Vision/datasets/internet_dataset_ObjectDiscovery-data/internet_processed/GT")
+    print(f"Internet_dataset: # samples = {len(Internet_dataset)}")
+
+
+    MSRC_dataset = MSRCDataset(image_dir="/Users/Sayan/Desktop/Research/IIT B/Vision/datasets/internet_dataset_ObjectDiscovery-data/MSRC_processed/images",
+                               mask_dir="/Users/Sayan/Desktop/Research/IIT B/Vision/datasets/internet_dataset_ObjectDiscovery-data/MSRC_processed/GT")
+    print(f"MSRC_dataset: # samples = {len(MSRC_dataset)}")
