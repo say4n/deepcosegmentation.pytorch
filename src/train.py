a1ce9ad6a17ce2ed7091c61fe9b309caa49b3331
Sayan Goswami <goswami.sayan47@gmail.com>
2018-07-11 19:32:00 +0530

    fixed!

--- src/train.py
+++ src/train.py
@@ -156,7 +156,7 @@ def train():
             intersection_a, intersection_b, union_a, union_b, precision_a, precision_b = 0, 0, 0, 0, 0, 0
 
             for idx in range(BATCH_SIZE//2):
-                pdb.set_trace()
+                # pdb.set_trace()
 
                 pred_maskA = np.uint64(pmapA_sq[idx].detach().cpu().numpy())
                 pred_maskB = np.uint64(pmapB_sq[idx].detach().cpu().numpy())
